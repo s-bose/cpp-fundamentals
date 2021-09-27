10e62e6d236427c35a614ec10a61a26b7f186850
Shiladitya Bose <shiladitya.31.z@gmail.com>
2021-09-27 18:24:07 +0200

    set 1 revision

--- part1/set1/07/main.cc
+++ part1/set1/07/main.cc
@@ -4,31 +4,29 @@ namespace
 {
     struct mask_t            // struct to define the bit sequence
     {                        // specifying bit size of each field
-        size_t a: 1;
-        size_t b: 3; 
-        size_t c: 4;
-        size_t d: 5;
-        size_t e: 4;
-        size_t f: 4;
-        size_t g: 4;
-        size_t h: 4;
-        size_t i: 4;
-        size_t j: 5;
-        size_t k: 2;        
+        size_t a: 1 = 0;     // and initializing with values
+        size_t b: 3 = 7; 
+        size_t c: 4 = 15;
+        size_t d: 5 = 10;
+        size_t e: 4 = 6;
+        size_t f: 4 = 7;
+        size_t g: 4 = 15;
+        size_t h: 4 = 15;
+        size_t i: 4 = 7;
+        size_t j: 5 = 0;
+        size_t k: 2 = 3;        
     };
 
     union bits_union         // union to store value
     {
-        size_t value;   
-                             // value is set by initializing the 
-                             // bit-fields of the struct
-        mask_t mask = {0, 7, 15, 10, 6, 7, 15, 15, 7, 0, 3};
+        size_t value;        // value holds mask's initializer defaults
+        mask_t mask{};
     };
 };
 
 int main()
 {
-    bits_union num;
-                            // output value as hexadecimal
-    std::cout << std::hex << num.value << '\n';
+                             // create anonymous default bits_union
+                             // object and display its value in hex
+    std::cout << std::hex << bits_union{}.value << '\n';
 }
--- part1/set1/08/main.cc
+++ part1/set1/08/main.cc
@@ -9,13 +9,15 @@ int main(int argc, char *argv[])
     size_t nibble = stoul(argv[2]);            // nibble to replace
     size_t replacement = stoul(argv[3]) % 16;  // new nibble (= 0 .. 15)
 
-    size_t mask = 15 << (4 * nibble);          // left-shift 15 (1111) by
-                                               // offset no of blocks
+    size_t shift = 4 * nibble;  // offset index to the replaced hex
 
-    mask = ((size_t) - 1) ^ mask;              // flip the mask
-    replacement = replacement << (4 * nibble); // move the replacement to its position
-    
-    value = (value & mask) | replacement;      // reset & replace the nibble with replacement 
+                                // xor size_t max (all 1) with 15 shifted
+                                // to nibble position to mask & reset nibble
+    size_t all_one = ~1 | 1;
+    size_t mask = all_one ^ (15 << shift);
 
-    cout << hex << value << '\n';              // show the new value
+    replacement = replacement << shift;   // move replacement to its position
+    value = (value & mask) | replacement; // reset & replace nibble
+
+    cout << hex << value << '\n';         // show the new value
 }
