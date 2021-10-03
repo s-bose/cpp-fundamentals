70bf93373dcd1fafb09433e054122cdf06b01338
shiladitya bose <bose@pop-os.localdomain>
2021-10-03 12:38:49 +0530

    set 1 tidied up w updated solutions

--- part1/set1/07/main.cc
+++ part1/set1/07/main.cc
@@ -4,17 +4,17 @@ namespace
 {
     struct mask_t            // struct to define the bit sequence
     {                        // specifying bit size of each field
-        size_t a: 1 = 0;     // and initializing with values
-        size_t b: 3 = 7; 
-        size_t c: 4 = 15;
-        size_t d: 5 = 10;
-        size_t e: 4 = 6;
-        size_t f: 4 = 7;
-        size_t g: 4 = 15;
-        size_t h: 4 = 15;
-        size_t i: 4 = 7;
-        size_t j: 5 = 0;
-        size_t k: 2 = 3;        
+        size_t bt_a: 1 = 0;     // and initializing with values
+        size_t bt_b: 3 = 7; 
+        size_t bt_c: 4 = 15;
+        size_t bt_d: 5 = 10;
+        size_t bt_e: 4 = 6;
+        size_t bt_f: 4 = 7;
+        size_t bt_g: 4 = 15;
+        size_t bt_h: 4 = 15;
+        size_t bt_i: 4 = 7;
+        size_t bt_j: 5 = 0;
+        size_t bt_k: 2 = 3;        
     };
 
     union bits_union         // union to store value
@@ -29,4 +29,7 @@ int main()
                              // create anonymous default bits_union
                              // object and display its value in hex
     std::cout << std::hex << bits_union{}.value << '\n';
+
+                             // also works
+    // std::cout << std::hex << bits_union{ .mask = mask_t{} }.value << '\n'; 
 }
--- part1/set1/08/main.cc
+++ part1/set1/08/main.cc
@@ -21,3 +21,24 @@ int main(int argc, char *argv[])
 
     cout << hex << value << '\n';         // show the new value
 }
+
+// alternate solution
+void alt_impl(int argc, char *argv[])
+{
+    size_t value = stoul(argv[1], 0, 16);   // initialize hexadecimal value
+    size_t nibble = stoul(argv[2]);         // nibble to replace
+    size_t replacement = stoul(argv[3]) % 16;   // new nibble (= 0 .. 15)
+
+        // 1: clear the intended nibble
+    size_t bitIdx = nibble * 4;         // bit index matching the nibble
+
+    value &= ~(15 << bitIdx);           // shift 15 (all nibble bits) 
+                                        // into place and toggle all bits.
+                                        // use & to clear the nibble 
+
+        // 2: assign the replacement to the correct nibble
+    value |= replacement << bitIdx;     // only set the replacement bits
+
+
+    cout << hex << value << '\n';       // show the new value
+}
