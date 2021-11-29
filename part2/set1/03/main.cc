4df77bdcf4d346aa1a23e762931c5680f0d2c4e4
sbose <shiladitya.31.z@gmail.com>
2021-11-30 01:32:24 +0530

    minor fix set1

--- part2/set1/03/main.cc
+++ part2/set1/03/main.cc
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <filesystem>
 
-int main(int argc, char *argv[]) {
-    if(argc != 3) {
-        std::cout << "Error with input arguments";
-    }
-    //Source: https://en.cppreference.com/w/cpp/filesystem/copy_file
-    std::filesystem::copy_file(argv[1],argv[2]);
+int main(int argc, char *argv[]) 
+{
+    if(argc != 3) 
+        std::cout << "Error with input arguments" << '\n';
+
+    std::filesystem::copy_file(argv[1], argv[2]);
+                                        // get last modified timestamp of first file
     std::filesystem::file_time_type lastWrite = std::filesystem::last_write_time(argv[1]);
-    std::filesystem::last_write_time(argv[2],lastWrite);
+
+                                        // replace second file's modified timestamp
+    std::filesystem::last_write_time(argv[2], lastWrite);
 }
