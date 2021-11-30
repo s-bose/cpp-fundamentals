bdd2d3ecccd9e96c3e9c17ea83e283a4a756f547
sbose <shiladitya.31.z@gmail.com>
2021-11-30 10:24:12 +0530

    final update before submission

--- part2/set1/03/main.cc
+++ part2/set1/03/main.cc
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <filesystem>
 
+using namespace std;
+
 int main(int argc, char *argv[]) 
 {
     if(argc != 3) 
-        std::cout << "Error with input arguments" << '\n';
+        cout << "Error with input arguments" << '\n';
 
-    std::filesystem::copy_file(argv[1], argv[2]);
-                                        // get last modified timestamp of first file
-    std::filesystem::file_time_type lastWrite = std::filesystem::last_write_time(argv[1]);
+    filesystem::copy_file(argv[1], argv[2]);
+                            // get last modified timestamp of first file
+    filesystem::file_time_type lastWrite = filesystem::last_write_time(argv[1]);
 
-                                        // replace second file's modified timestamp
-    std::filesystem::last_write_time(argv[2], lastWrite);
+                            // replace second file's modified timestamp
+    filesystem::last_write_time(argv[2], lastWrite);
 }
