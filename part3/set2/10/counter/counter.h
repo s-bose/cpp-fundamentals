8c7021dee64f29bb9da5db56310b7e88c2ed41b2
shiladitya bose <shiladitya_basu@live.com>
2022-02-22 23:55:25 +0530

    #10 done

---
+++ part3/set2/10/counter/counter.h
@@ -0,0 +1,112 @@
+#ifndef INCLUDED_COUNTER_
+#define INCLUDED_COUNTER_
+
+#include <memory>
+#include <ostream>
+
+template <typename Type>
+class Counter
+{
+    static size_t s_count;
+    static size_t s_actual;
+    std::shared_ptr<Type> d_data;
+
+    public:
+        Counter();
+        Counter(Type const &value);
+        Counter(Counter<Type> const &obj);
+        Counter(Counter<Type> &&tmp);
+        ~Counter();
+
+        static size_t count();
+        static size_t actual();
+
+        Counter<Type> &operator++();
+
+        Type get() const;
+
+        template <typename Tp>
+        friend std::ostream &operator<<(std::ostream &, 
+                                       Counter<Tp> const &);
+};
+
+template <typename Type>
+Counter<Type>::Counter()
+:
+    d_data(std::make_shared<Type>(Type{}))
+{
+    ++s_count;
+    ++s_actual;
+}
+
+template <typename Type>
+Counter<Type>::Counter(Type const &value)
+: 
+    d_data(std::make_shared<Type>(value))
+{
+    ++s_count;
+    ++s_actual;
+}
+
+template <typename Type>
+Counter<Type>::Counter(Counter<Type> const &obj)
+:
+    d_data(obj.d_data)
+{
+    ++s_count;
+    ++s_actual;
+}
+
+template <typename Type>
+Counter<Type>::Counter(Counter<Type> &&tmp)
+:
+    d_data(std::move(tmp.d_data))
+{
+    ++s_count;
+    ++s_actual;
+}
+
+template <typename Type>
+size_t Counter<Type>::count()
+{
+    return s_count;
+}
+
+template <typename Type>
+size_t Counter<Type>::actual()
+{
+    return s_actual;
+}
+
+template <typename Type>
+Counter<Type> &Counter<Type>::operator++()
+{
+    ++*d_data;
+    return *this;
+}
+
+template <typename Type>
+Type Counter<Type>::get() const
+{
+    return *d_data;
+}
+
+template <typename Tp>
+std::ostream &operator<<(std::ostream &out, Counter<Tp> const &obj)
+{
+    return out << *obj.d_data;
+}
+
+template <typename Type>
+Counter<Type>::~Counter()
+{
+    --s_actual;
+}
+
+template <typename Type>
+size_t Counter<Type>::s_count = 0;
+
+template <typename Type>
+size_t Counter<Type>::s_actual = 0;
+
+#endif
