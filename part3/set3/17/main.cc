9a4ec57bdf79d366197fa74535462f56ed76929e
shiladitya bose <shiladitya_basu@live.com>
2022-03-14 14:01:27 +0530

    set3 second submission

--- part3/set3/17/main.cc
+++ part3/set3/17/main.cc
@@ -9,5 +9,5 @@ int main()
     cout << NrTrait<1971962>::value << '\n'
          << NrTrait<1971962>::even << '\n'
          << NrTrait<1971962>::by3 << '\n'
-         << NrTrait<1971962>::width << '\n';
+         << NrTrait<0>::width << '\n';
 }
--- part3/set3/17/nrtrait/nrtrait.h
+++ part3/set3/17/nrtrait/nrtrait.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDED_NRTRAIT_
 #define INCLUDED_NRTRAIT_
 
-#include <iosfwd>
+#include <cstddef>
 #include <cmath>
 
 template <size_t Nr>
@@ -9,12 +9,23 @@ struct NrTrait
 {
                 // evaluate expr compile-time
     static constexpr size_t value = Nr;
-    static constexpr bool even = not (Nr % 2);
-    static constexpr bool by3 = not (Nr % 3);
+    static constexpr bool even = Nr % 2 == 0;
+    static constexpr bool by3 = Nr % 3 == 0;
     
-                // find no. of digits 
-    static constexpr size_t width = std::ceil(std::log10(Nr));
+    static constexpr size_t width = Nr == 0 ? 1 : NrTrait<Nr>::getWidth();
+ 
+  private:
+    consteval static size_t getWidth();
 };
 
-        
+template <size_t Nr> // compile-time eval width of non-zero Nr
+consteval size_t NrTrait<Nr>::getWidth()
+{
+  size_t size = 0;
+  for (size_t num = value; num != 0; num /= 10, ++size);
+
+  return size;
+} 
+
+
 #endif
